7c889b6ad50687f99a122a6aacf9c2f89eceed3a
Adrian King Legaspi <aki.legaspi@gmail.com>
2015-04-15 12:17:53 +0800

    Design shits and updated to sql file

--- Thesis/Login.vb
+++ Thesis/Login.vb
@@ -1,13 +1,26 @@
-﻿
+﻿Imports MySql.Data.MySqlClient
+
+
 Public Class LogIn
+    Private cs As String = "Database=hmsystem; Data Source=localhost; User Id=root; Password=nothing"
+    Private conn As New MySqlConnection(cs)
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        Try
+            conn.Open()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            conn.Close()
+        End Try
     End Sub
 
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        SignUp.Show()
+        Dim cmd As MySqlCommand
+        Dim scm As String = "SELECT VERSION()"
+        cmd = New MySqlCommand(scm, conn)
+        MsgBox(Convert.ToString(cmd.ExecuteScalar()))
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
